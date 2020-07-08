b4c279085b403d9c80c83be3f5337fb14bc68838
vanusasaravalle <vanusa.sousasaravalle@gmail.com>
2020-07-08 09:18:31 -0300

    Departamento

---
+++ src/main/java/application/Programa.java
@@ -0,0 +1,12 @@
+package application;
+
+import model.Departamento;
+
+public class Programa {
+
+    public static void main(String[] args) {
+
+        Departamento obj = new Departamento(1,"Books");
+        System.out.println(obj);
+    }
+}
---
+++ src/main/java/model/Departamento.java
@@ -0,0 +1,53 @@
+package model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Departamento  implements Serializable {
+
+
+    private Integer id;
+    private String name;
+
+    public Departamento(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Departamento)) return false;
+        Departamento that = (Departamento) o;
+        return getId().equals(that.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+
+    @Override
+    public String toString() {
+        return "Departamento{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
