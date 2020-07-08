5d87531db66b5c6523a1cb582cee5ee8f9b5a680
vanusasaravalle <vanusa.sousasaravalle@gmail.com>
2020-07-08 09:29:01 -0300

    Vendedor

--- src/main/java/application/Programa.java
+++ src/main/java/application/Programa.java
@@ -1,12 +1,18 @@
 package application;
 
 import model.Departamento;
+import model.Vendedor;
+
+import java.util.Date;
 
 public class Programa {
 
     public static void main(String[] args) {
 
         Departamento obj = new Departamento(1,"Books");
-        System.out.println(obj);
+
+        Vendedor vendedor = new Vendedor(21, "Bob","bob@gmail.com", new Date(),3000.0, obj);
+
+        System.out.println(vendedor);
     }
 }
---
+++ src/main/java/model/Vendedor.java
@@ -0,0 +1,105 @@
+package model;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+
+public class Vendedor implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private Integer id;
+    private String name;
+    private String email;
+    private Date nascimento;
+    private Double salario;
+
+    private Departamento departamento;
+
+    public Vendedor(Integer id, String name, String email, Date nascimento, Double salario, Departamento departamento) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.nascimento = nascimento;
+        this.salario = salario;
+        this.departamento = departamento;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getNascimento() {
+        return nascimento;
+    }
+
+    public void setNascimento(Date nascimento) {
+        this.nascimento = nascimento;
+    }
+
+    public Double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(Double salario) {
+        this.salario = salario;
+    }
+
+    public Departamento getDepartamento() {
+        return departamento;
+    }
+
+    public void setDepartamento(Departamento departamento) {
+        this.departamento = departamento;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Vendedor)) return false;
+        Vendedor vendedor = (Vendedor) o;
+        return getId().equals(vendedor.getId()) &&
+                getName().equals(vendedor.getName()) &&
+                getEmail().equals(vendedor.getEmail()) &&
+                getNascimento().equals(vendedor.getNascimento()) &&
+                getSalario().equals(vendedor.getSalario()) &&
+                getDepartamento().equals(vendedor.getDepartamento());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getName(), getEmail(), getNascimento(), getSalario(), getDepartamento());
+    }
+
+    @Override
+    public String toString() {
+        return "Vendedor{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", nascimento=" + nascimento +
+                ", salario=" + salario +
+                ", departamento=" + departamento +
+                '}';
+    }
+}
